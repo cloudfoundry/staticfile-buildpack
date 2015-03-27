7cec182d2d841ae2d82e2a168c3fe50b99d4d903
Dr Nic Williams <drnicwilliams@gmail.com>
2015-03-27 10:01:47 -0700

    now testing both cflinuxfs2 lucid64 stacks

--- test/test.sh
+++ test/test.sh
@@ -10,12 +10,13 @@ REPO=${REPO:="staticfile-buildpack"}
 BRANCH=${BRANCH:="master"}
 buildpack="https://github.com/$ORG/$REPO#$BRANCH"
 
-stacks=( lucid64 )
-for stack in $stacks; do
+stacks=(lucid64 cflinuxfs2)
+for stack in ${stacks[*]}; do
   for test_app in test/fixtures/*; do
     name=$(basename $test_app)
     cf push $name -p $test_app -b $buildpack -s $stack --random-route
     cf open $name
+    cf d $name -f
   done
 done
 
