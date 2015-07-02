507f9a1d16cd34f374bea2470bca42585f9078b4
Colin Jackson <colin.s.jackson90@gmail.com>
2015-07-02 12:03:16 -0400

    Compress nginx response body for more MIME types
    [#98128132]

--- cf_spec/integration/deploy_staticfile_app_spec.rb
+++ cf_spec/integration/deploy_staticfile_app_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'excon'
 
 describe 'deploy a staticfile app' do
   let(:app) { Machete.deploy_app('staticfile_app') }
@@ -17,5 +18,10 @@ describe 'deploy a staticfile app' do
 
     browser.visit_path('/fixture.json')
     expect(browser.content_type).to eq('application/json')
+
+    response = Excon.get("http://#{browser.base_url}/lots_of.js", headers: {
+      'Accept-Encoding' => 'gzip'
+    })
+    expect(response.headers['Content-Encoding']).to eq('gzip')
   end
 end
