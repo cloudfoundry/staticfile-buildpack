8a93ee948431c8f21c11294974783022f0a20cf4
JT Archie <jtarchie@gmail.com>
2016-03-15 15:31:39 -0400

    toggle for ssi in Staticfile [#111667592]

---
+++ cf_spec/integration/deploy_ssi_app_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'deploy a staticfile app' do
+  let(:browser) { Machete::Browser.new(app) }
+  let(:timestamp) { Time.now.to_s }
+  let(:body) { "I feel included!" }
+
+  after do
+    Machete::CF::DeleteApp.new.execute(app)
+  end
+
+  context 'ssi is toggled on' do
+    let(:app) { Machete.deploy_app('ssi_enabled_app') }
+
+    specify do
+      expect(app).to be_running
+
+      browser.visit_path('/')
+      expect(browser).to have_body(body)
+      expect(browser).to_not have_body('<!--# include file="ssi_body.html" -->')
+    end
+  end
+
+  context 'ssi is toggled off' do
+    let(:app) { Machete.deploy_app('ssi_disabled_app') }
+
+    specify do
+      expect(app).to be_running
+
+      browser.visit_path('/')
+      expect(browser).to_not have_body(body)
+      expect(browser).to have_body('<!--# include file="ssi_body.html" -->')
+    end
+  end
+end
