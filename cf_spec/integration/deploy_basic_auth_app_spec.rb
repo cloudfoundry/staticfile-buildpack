078ad893d9a7280749c051a9fca57fcbfd5e89e8
Sam Smith <sesmith177@gmail.com>
2016-11-02 10:56:57 -0400

    Don't write .htpasswd to the logs
    - correct broken Github link
    - add 2nd user:passwd pair to Staticfile.auth fixture
    - remove line that writes .htpasswd to the logs
    
    [#130048269]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- cf_spec/integration/deploy_basic_auth_app_spec.rb
+++ cf_spec/integration/deploy_basic_auth_app_spec.rb
@@ -1,23 +1,37 @@
 require 'spec_helper'
 
 describe 'deploy a basic auth app' do
-  let(:app) { Machete.deploy_app('basic_auth') }
-  let(:browser) { Machete::Browser.new(app) }
+  let(:app_name) { 'override'}
+  let(:app)      { Machete.deploy_app(app_name) }
+  let(:browser)  { Machete::Browser.new(app) }
 
   after do
     Machete::CF::DeleteApp.new.execute(app)
   end
 
-  specify do
-    expect(app).to be_running
+  context 'the app uses Staticfile.auth' do
+    let(:app_name) { 'basic_auth' }
 
-    browser.visit_path('/', username: 'bob', password: 'bob')
-    expect(browser).to have_body('This site is protected by basic auth. User: <code>bob</code>; Password: <code>bob</code>.')
+    it 'uses the provided credentials for authorization' do
+      expect(app).to be_running
 
-    browser.visit_path('/')
-    expect(browser).to have_body('401 Authorization Required')
+      browser.visit_path('/', username: 'bob', password: 'bob')
+      expect(browser).to have_body('This site is protected by basic auth. User: <code>bob</code>; Password: <code>bob</code>.')
 
-    browser.visit_path('/', username: 'bob', password: 'bob1')
-    expect(browser).to have_body('401 Authorization Required')
+      browser.visit_path('/')
+      expect(browser).to have_body('401 Authorization Required')
+
+      browser.visit_path('/', username: 'bob', password: 'bob1')
+      expect(browser).to have_body('401 Authorization Required')
+    end
+
+    it 'does not write the contents of .htpasswd to the logs' do
+      expect(app).not_to have_logged('bob:$apr1$DuUQEQp8$ZccZCHQElNSjrg.erwSFC0')
+      expect(app).not_to have_logged('dave:$apr1$oupuwqML$5Gq.yX2thmaz2ORfx9.v4.')
+    end
+
+    it 'logs the source of authentication credentials' do
+      expect(app).to have_logged('-----> Enabling basic authentication using Staticfile.auth')
+    end
   end
 end
