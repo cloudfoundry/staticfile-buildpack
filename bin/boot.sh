6399519f3b30b6a6f5fd3c897e4bb759d22c0913
hyayi <hyayi@us.ibm.com>
2017-01-26 16:40:34 -0500

    added echo

--- bin/boot.sh
+++ bin/boot.sh
@@ -12,6 +12,7 @@
 # and  limitations under the License.
 # ------------------------------------------------------------------------------------------------
 echo "====> in boot.sh"
+echo $HOME
 export APP_ROOT=$HOME
 # export LD_LIBRARY_PATH=$APP_ROOT/nginx/lib:$LD_LIBRARY_PATH
 
@@ -27,11 +28,11 @@ mv $conf_file $APP_ROOT/openresty/nginx/conf/orig.conf
 erb $APP_ROOT/openresty/nginx/conf/orig.conf > $APP_ROOT/openresty/nginx/conf/nginx.conf
 
 
-# mkdir -p $APP_ROOT/nginx/nginx/client_body_temp
-# mkdir -p $APP_ROOT/nginx/logs
-# cat > $APP_ROOT/nginx/logs/error.log
-# cat > $APP_ROOT/nginx/nginx/error.log
-# cat > $APP_ROOT/nginx/nginx/access.log
+mkdir -p $APP_ROOT/nginx/nginx/client_body_temp
+mkdir -p $APP_ROOT/nginx/logs
+cat > $APP_ROOT/nginx/logs/error.log
+cat > $APP_ROOT/nginx/nginx/error.log
+cat > $APP_ROOT/nginx/nginx/access.log
 # ------------------------------------------------------------------------------------------------
 
 # mkfifo $APP_ROOT/openresty/nginx/logs/access.log
