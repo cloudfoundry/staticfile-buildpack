c7a9fcd21cc331ec3e2b5cd13d89da20d4e1e023
Forest Eckhardt <forest.eckhardt@gmail.com>
2017-08-10 15:57:31 -0400

    Update integration tests to use shared Cutlass test helpers
    [#150150212]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- src/staticfile/integration/integration_suite_test.go
+++ src/staticfile/integration/integration_suite_test.go
@@ -1,16 +1,12 @@
 package integration_test
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"time"
 
-	"github.com/cloudfoundry/libbuildpack"
 	"github.com/cloudfoundry/libbuildpack/cutlass"
-	"github.com/cloudfoundry/libbuildpack/packager"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,6 +16,7 @@ import (
 
 var bpDir string
 var buildpackVersion string
+var packagedBuildpack cutlass.VersionedBuildpackPackage
 
 func init() {
 	flag.StringVar(&buildpackVersion, "version", "", "version to use (builds if empty)")
@@ -29,6 +26,42 @@ func init() {
 	flag.Parse()
 	fmt.Println("cutlass.Cached", cutlass.Cached)
 }
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	// Run once
+	if buildpackVersion == "" {
+		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack()
+		Expect(err).NotTo(HaveOccurred())
+
+		data, err := json.Marshal(packagedBuildpack)
+		Expect(err).NotTo(HaveOccurred())
+		return data
+	}
+
+	return []byte{}
+}, func(data []byte) {
+	// Run on all nodes
+	var err error
+	if len(data) > 0 {
+		err = json.Unmarshal(data, &packagedBuildpack)
+		Expect(err).NotTo(HaveOccurred())
+		buildpackVersion = packagedBuildpack.Version
+	}
+
+	bpDir, err = cutlass.FindRoot()
+	Expect(err).NotTo(HaveOccurred())
+
+	cutlass.SeedRandom()
+	cutlass.DefaultStdoutStderr = GinkgoWriter
+})
+
+var _ = SynchronizedAfterSuite(func() {
+	// Run on all nodes
+}, func() {
+	// Run once
+	Expect(cutlass.RemovePackagedBuildpack(packagedBuildpack)).To(Succeed())
+	Expect(cutlass.DeleteOrphanedRoutes()).To(Succeed())
+})
 
 func TestIntegration(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -40,53 +73,3 @@ func PushAppAndConfirm(app *cutlass.App) {
 	Eventually(func() ([]string, error) { return app.InstanceStates() }, 10*time.Second).Should(Equal([]string{"RUNNING"}))
 	Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
 }
-
-func findRoot() string {
-	file := "VERSION"
-	for {
-		files, err := filepath.Glob(file)
-		Expect(err).To(BeNil())
-		if len(files) == 1 {
-			file, err = filepath.Abs(filepath.Dir(file))
-			Expect(err).To(BeNil())
-			return file
-		}
-		file = filepath.Join("..", file)
-	}
-}
-
-var _ = SynchronizedBeforeSuite(func() []byte {
-	// Run once
-	bpDir = findRoot()
-
-	if buildpackVersion == "" {
-		data, err := ioutil.ReadFile(filepath.Join(bpDir, "VERSION"))
-		Expect(err).NotTo(HaveOccurred())
-		buildpackVersion = string(data)
-		buildpackVersion = fmt.Sprintf("%s.%s", buildpackVersion, time.Now().Format("20060102150405"))
-
-		file, err := packager.Package(bpDir, packager.CacheDir, buildpackVersion, cutlass.Cached)
-		Expect(err).To(BeNil())
-
-		var manifest struct {
-			Language string `yaml:"language"`
-		}
-		Expect(libbuildpack.NewYAML().Load(filepath.Join(bpDir, "manifest.yml"), &manifest)).To(Succeed())
-		Expect(cutlass.UpdateBuildpack(manifest.Language, file)).To(Succeed())
-
-		os.Remove(file)
-	}
-
-	return nil
-}, func(_ []byte) {
-	// Run on all nodes
-	bpDir = findRoot()
-	cutlass.DefaultStdoutStderr = GinkgoWriter
-})
-
-var _ = SynchronizedAfterSuite(func() {
-	// Run on all nodes
-}, func() {
-	// Run once
-	cutlass.DeleteOrphanedRoutes()
-})
