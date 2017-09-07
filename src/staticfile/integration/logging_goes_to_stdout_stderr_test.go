595efba76bd1d93befa08458c2e0e1f42eb7dbbd
Dave Goddard <dave@goddard.id.au>
2017-09-07 14:00:18 -0400

    Pass function to eventually in test (not value)

--- src/staticfile/integration/logging_goes_to_stdout_stderr_test.go
+++ src/staticfile/integration/logging_goes_to_stdout_stderr_test.go
@@ -3,6 +3,7 @@ package integration_test
 import (
 	"os/exec"
 	"path/filepath"
+	"time"
 
 	"github.com/cloudfoundry/libbuildpack/cutlass"
 
@@ -27,14 +28,14 @@ var _ = Describe("nginx logs go to stdout and stderr", func() {
 	It("", func() {
 		By("writes regular logs to stdout and does not write to actual log files", func() {
 			Expect(app.GetBody("/")).To(ContainSubstring("This is an example app for Cloud Foundry that is only static HTML/JS/CSS assets."))
-			Eventually(app.Stdout.String(), 5*time.Second).Should(MatchRegexp("OUT.*GET / HTTP/1.1"))
+			Eventually(app.Stdout.String, 5*time.Second).Should(MatchRegexp("OUT.*GET / HTTP/1.1"))
 			command := exec.Command("cf", "ssh", app.Name, "-c", "ls -l /app/nginx/logs/ | grep access.log")
 			Expect(command.Output()).To(ContainSubstring(" vcap 0 "))
 		})
 
 		By("writes error logs to stderr and does not write to actual log files", func() {
 			Expect(app.GetBody("/idontexist")).To(ContainSubstring("404 Not Found"))
-			Eventually(app.Stdout.String(), 5*time.Second).Should(MatchRegexp("ERR.*GET /idontexist HTTP/1.1"))
+			Eventually(app.Stdout.String, 5*time.Second).Should(MatchRegexp("ERR.*GET /idontexist HTTP/1.1"))
 			command := exec.Command("cf", "ssh", app.Name, "-c", "ls -l /app/nginx/logs/ | grep error.log")
 			Expect(command.Output()).To(ContainSubstring(" vcap 0 "))
 		})
