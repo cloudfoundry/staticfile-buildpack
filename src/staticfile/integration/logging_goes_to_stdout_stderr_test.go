1b3d5ab53594083384aafa7585761664ec00c6e4
Dave Goddard <dave@goddard.id.au>
2017-09-07 12:06:48 -0400

    Make failures easier to read than []byte
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- src/staticfile/integration/logging_goes_to_stdout_stderr_test.go
+++ src/staticfile/integration/logging_goes_to_stdout_stderr_test.go
@@ -27,14 +27,14 @@ var _ = Describe("nginx logs go to stdout and stderr", func() {
 	It("", func() {
 		By("writes regular logs to stdout and does not write to actual log files", func() {
 			Expect(app.GetBody("/")).To(ContainSubstring("This is an example app for Cloud Foundry that is only static HTML/JS/CSS assets."))
-			Eventually(app.Stdout).Should(MatchRegexp("OUT.*GET / HTTP/1.1"))
+			Eventually(app.Stdout.String()).Should(MatchRegexp("OUT.*GET / HTTP/1.1"))
 			command := exec.Command("cf", "ssh", app.Name, "-c", "ls -l /app/nginx/logs/ | grep access.log")
 			Expect(command.Output()).To(ContainSubstring(" vcap 0 "))
 		})
 
 		By("writes error logs to stderr and does not write to actual log files", func() {
 			Expect(app.GetBody("/idontexist")).To(ContainSubstring("404 Not Found"))
-			Eventually(app.Stdout).Should(MatchRegexp("ERR.*GET /idontexist HTTP/1.1"))
+			Eventually(app.Stdout.String()).Should(MatchRegexp("ERR.*GET /idontexist HTTP/1.1"))
 			command := exec.Command("cf", "ssh", app.Name, "-c", "ls -l /app/nginx/logs/ | grep error.log")
 			Expect(command.Output()).To(ContainSubstring(" vcap 0 "))
 		})
