a639a4400bf8278b66f98fcd6262e3aad90e4321
Rahul Krishna Upadhyaya <rakrup@gmail.com>
2017-09-21 19:34:28 +0530

    Fixes #118 : Fizes issue in running tests on MacOS

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -2,7 +2,16 @@
 set -euo pipefail
 set -x
 
-export ROOT=$(dirname $(readlink -f ${BASH_SOURCE%/*}))
+unamestr=`uname`
+# checks rhe operating system to issue readlink on Linux and 
+# stat command on Mac OS.
+if [[ "$unamestr" == 'Linux' ]]; then
+	readlinkalias='readlink '
+elif [[ "$unamestr" == 'Darwin' ]]; then
+        readlinkalias='stat '
+fi
+
+export ROOT=$(dirname $($readlinkalias -f ${BASH_SOURCE%/*}))
 if [ ! -f "$ROOT/.bin/ginkgo" ]; then
   (cd "$ROOT/src/staticfile/vendor/github.com/onsi/ginkgo/ginkgo/" && go install)
 fi
--- scripts/unit.sh
+++ scripts/unit.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
+unamestr=`uname`
+# checks rhe operating system to issue readlink on Linux and 
+# stat command on Mac OS.
+if [[ "$unamestr" == 'Linux' ]]; then
+        readlinkalias='readlink '
+elif [[ "$unamestr" == 'Darwin' ]]; then
+        readlinkalias='stat '
+fi
 
-export ROOT=`dirname $(readlink -f ${BASH_SOURCE%/*})`
+export ROOT=`dirname $($readlinkalias -f ${BASH_SOURCE%/*})`
 if [ ! -f $ROOT/.bin/ginkgo ]; then
   (cd $ROOT/src/staticfile/vendor/github.com/onsi/ginkgo/ginkgo/ && go install)
 fi
