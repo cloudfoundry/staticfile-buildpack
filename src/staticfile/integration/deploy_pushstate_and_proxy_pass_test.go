b0e87363400748f638dc5ef663babb99b350f322
Dave Goddard <dave@goddard.id.au>
2017-11-01 10:25:32 -0400

    Adding pushstate_and_proxy_pass test [#152312644]

---
+++ src/staticfile/integration/deploy_pushstate_and_proxy_pass_test.go
@@ -0,0 +1,37 @@
+package integration_test
+
+import (
+	"path/filepath"
+
+	"github.com/cloudfoundry/libbuildpack/cutlass"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("deploy a pushstate and reverse proxy app", func() {
+	var app *cutlass.App
+	AfterEach(func() {
+		if app != nil {
+			app.Destroy()
+		}
+		app = nil
+	})
+
+	BeforeEach(func() {
+		app = cutlass.New(filepath.Join(bpDir, "fixtures", "pushstate_and_proxy_pass"))
+		PushAppAndConfirm(app)
+	})
+
+	It("", func() {
+		By("enables pushstate", func() {
+			Expect(app.GetBody("/")).To(ContainSubstring("This is the index file"))
+			Expect(app.GetBody("/static.html")).To(ContainSubstring("This is a static file"))
+			Expect(app.GetBody("/unknown")).To(ContainSubstring("This is the index file"))
+		})
+
+		By("proxies", func() {
+			Expect(app.GetBody("/api")).To(ContainSubstring("This domain is established to be used for illustrative examples in documents"))
+		})
+	})
+})
