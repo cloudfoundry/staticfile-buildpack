cc45ccde6971626d0d189b84a8ef4a3ba26545a0
Dave Goddard <dave@goddard.id.au>
2017-11-16 08:20:00 -0500

    Convert to libbuildpack brats [#152922670]

---
+++ scripts/brats.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+source .envrc
+./scripts/install_tools.sh
+
+GINKGO_NODES=${GINKGO_NODES:-3}
+GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-2}
+
+cd src/*/brats
+
+echo "Run Buildpack Runtime Acceptance Tests"
+ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES
---
+++ src/staticfile/brats/brats_test.go
@@ -0,0 +1,14 @@
+package brats_test
+
+import (
+	"github.com/cloudfoundry/libbuildpack/bratshelper"
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Staticfile buildpack", func() {
+	bratshelper.UnbuiltBuildpack("nginx", CopyBrats)
+	bratshelper.DeployingAnAppWithAnUpdatedVersionOfTheSameBuildpack(CopyBrats)
+	bratshelper.StagingWithCustomBuildpackWithCredentialsInDependencies(`nginx\-[\d\.]+\-linux\-x64\-[\da-f]+\.tgz`, CopyBrats)
+	bratshelper.DeployAppWithExecutableProfileScript("nginx", CopyBrats)
+	bratshelper.DeployAnAppWithSensitiveEnvironmentVariables(CopyBrats)
+})
