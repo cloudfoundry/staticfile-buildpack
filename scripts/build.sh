bf348668914bd727d218a3189b76cd6090eaac39
Sam Coward <scoward@pivotal.io>
2017-12-13 10:19:12 -0500

    Use GOOS=linux when building
    Signed-off-by: Brendan Good <brendantgood@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ set -exuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
-go build -ldflags="-s -w" -o bin/supply staticfile/supply/cli
-go build -ldflags="-s -w" -o bin/finalize staticfile/finalize/cli
+GOOS=linux go build -ldflags="-s -w" -o bin/supply staticfile/supply/cli
+GOOS=linux go build -ldflags="-s -w" -o bin/finalize staticfile/finalize/cli
