d55d84440e983d11af9c5f91b3c3f54de0afe2a7
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2018-02-14 20:55:59 +0000

    Update libbuildpack

--- src/staticfile/vendor/github.com/cloudfoundry/libbuildpack/packager/buildpack-packager/main.go
+++ src/staticfile/vendor/github.com/cloudfoundry/libbuildpack/packager/buildpack-packager/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -9,49 +10,88 @@ import (
 	"strings"
 
 	"github.com/cloudfoundry/libbuildpack/packager"
+	"github.com/google/subcommands"
 )
 
-func main() {
-	var cached, summary bool
-	var version, cacheDir string
+type summaryCmd struct {
+}
 
-	flag.StringVar(&version, "version", "", "version to build as")
-	flag.BoolVar(&cached, "cached", false, "include dependencies")
-	flag.BoolVar(&summary, "summary", false, "list dependencies")
-	flag.StringVar(&cacheDir, "cachedir", packager.CacheDir, "cache dir")
-	flag.Parse()
-
-	if summary {
-		if summary, err := packager.Summary("."); err != nil {
-			log.Fatalf("error: %v", err)
-		} else {
-			fmt.Println(summary)
-		}
-		os.Exit(0)
+func (*summaryCmd) Name() string             { return "summary" }
+func (*summaryCmd) Synopsis() string         { return "Print out list of dependencies of this buildpack" }
+func (*summaryCmd) SetFlags(f *flag.FlagSet) {}
+func (*summaryCmd) Usage() string {
+	return `summary:
+  When run in a directory that is structured as a buildpack, prints a list of depedencies of that buildpack.
+  (i.e. what would be downloaded to build a cached zipfile)
+`
+}
+func (s *summaryCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	summary, err := packager.Summary(".")
+	if err != nil {
+		log.Printf("error reading dependencies from manifest: %v", err)
+		return subcommands.ExitFailure
 	}
+	fmt.Println(summary)
+	return subcommands.ExitSuccess
+}
 
-	if version == "" {
+type buildCmd struct {
+	cached   bool
+	version  string
+	cacheDir string
+}
+
+func (*buildCmd) Name() string     { return "build" }
+func (*buildCmd) Synopsis() string { return "Create a buildpack zipfile from the current directory" }
+func (*buildCmd) Usage() string {
+	return `build [-cached] [-version <version>] [-cachedir <path to cachedir>]:
+  When run in a directory that is structured as a buildpack, creates a zip file.
+
+`
+}
+func (b *buildCmd) SetFlags(f *flag.FlagSet) {
+	f.StringVar(&b.version, "version", "", "version to build as")
+	f.BoolVar(&b.cached, "cached", false, "include dependencies")
+	f.StringVar(&b.cacheDir, "cachedir", packager.CacheDir, "cache dir")
+}
+func (b *buildCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	if b.version == "" {
 		v, err := ioutil.ReadFile("VERSION")
 		if err != nil {
-			log.Fatalf("error: Could not read VERSION file: %v", err)
+			log.Printf("error: Could not read VERSION file: %v", err)
+			return subcommands.ExitFailure
 		}
-		version = strings.TrimSpace(string(v))
+		b.version = strings.TrimSpace(string(v))
 	}
 
-	zipFile, err := packager.Package(".", cacheDir, version, cached)
+	zipFile, err := packager.Package(".", b.cacheDir, b.version, b.cached)
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		log.Printf("error while creating zipfile: %v", err)
+		return subcommands.ExitFailure
 	}
 
 	buildpackType := "uncached"
-	if cached {
+	if b.cached {
 		buildpackType = "cached"
 	}
 
 	stat, err := os.Stat(zipFile)
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		log.Printf("error while stating zipfile: %v", err)
+		return subcommands.ExitFailure
 	}
 
 	fmt.Printf("%s buildpack created and saved as %s with a size of %dMB\n", buildpackType, zipFile, stat.Size()/1024/1024)
+	return subcommands.ExitSuccess
+}
+func main() {
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(subcommands.FlagsCommand(), "")
+	subcommands.Register(subcommands.CommandsCommand(), "")
+	subcommands.Register(&summaryCmd{}, "Custom")
+	subcommands.Register(&buildCmd{}, "Custom")
+
+	flag.Parse()
+	ctx := context.Background() // TODO what is this even??
+	os.Exit(int(subcommands.Execute(ctx)))
 }
