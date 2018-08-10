e5e8ecd71f322e5b7cd16f3a930217aa8734d404
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2018-08-10 13:44:56 +0000

    Update libbuildpack

--- src/staticfile/vendor/github.com/cloudfoundry/libbuildpack/bratshelper/tests.go
+++ src/staticfile/vendor/github.com/cloudfoundry/libbuildpack/bratshelper/tests.go
@@ -196,14 +196,33 @@ func StagingWithADepThatIsNotTheLatest(depName string, copyBrats func(string) *c
 func StagingWithCustomBuildpackWithCredentialsInDependencies(depRegexp string, copyBrats func(string) *cutlass.App) {
 	Describe("staging with custom buildpack that uses credentials in manifest dependency uris", func() {
 		var (
-			buildpackFile, bpName, stack string
-			app                          *cutlass.App
+			buildpackFile, bpName, stack, username, password string
+			app                                              *cutlass.App
 		)
 		JustBeforeEach(func() {
 			file, err := ModifyBuildpackManifest(buildpackFile, func(m *Manifest) {
 				for _, d := range m.Dependencies {
 					uri, err := url.Parse(d.URI)
-					uri.User = url.UserPassword("login", "password")
+					if proxyHost, ok := os.LookupEnv("PROXY_HOST"); ok {
+						uri.Host = proxyHost
+					}
+					if proxyPort, ok := os.LookupEnv("PROXY_PORT"); ok {
+						uri.Host += ":" + proxyPort
+					}
+					if proxyScheme, ok := os.LookupEnv("PROXY_SCHEME"); ok {
+						uri.Scheme = proxyScheme
+					}
+					if proxyUsername, ok := os.LookupEnv("PROXY_USERNAME"); ok {
+						username = proxyUsername
+					} else {
+						username = "login"
+					}
+					if proxyPassword, ok := os.LookupEnv("PROXY_PASSWORD"); ok {
+						password = proxyPassword
+					} else {
+						password = "password"
+					}
+					uri.User = url.UserPassword(username, password)
 					Expect(err).ToNot(HaveOccurred())
 					d.URI = uri.String()
 				}
@@ -236,8 +255,8 @@ func StagingWithCustomBuildpackWithCredentialsInDependencies(depRegexp string, c
 			})
 			It("does not include credentials in logged dependency uris", func() {
 				Expect(app.Stdout.String()).To(MatchRegexp(depRegexp))
-				Expect(app.Stdout.String()).ToNot(ContainSubstring("login"))
-				Expect(app.Stdout.String()).ToNot(ContainSubstring("password"))
+				Expect(app.Stdout.String()).ToNot(ContainSubstring(username))
+				Expect(app.Stdout.String()).ToNot(ContainSubstring(password))
 			})
 		})
 	})
