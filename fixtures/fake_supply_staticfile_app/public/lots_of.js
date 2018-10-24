585b273e0d2ffd7767bdfa496a63d98409d8ec23
Danny Joyce <djoyce@pivotal.io>
2018-10-24 16:28:46 -0400

    Add multi-buildpack tests [#149434941]
    Co-authored-by: Daniel Thornton <dthornton@pivotal.io>

---
+++ fixtures/fake_supply_staticfile_app/public/lots_of.js
@@ -0,0 +1,272 @@
+[
+    {
+        "_id": "55944421f8bab4440b7807ed",
+        "index": 0,
+        "guid": "c1712c69-a83b-437a-868d-82906fe8015a",
+        "isActive": false,
+        "balance": "$2,387.27",
+        "picture": "http://placehold.it/32x32",
+        "age": 29,
+        "eyeColor": "blue",
+        "name": "Kristine Raymond",
+        "gender": "female",
+        "company": "PYRAMAX",
+        "email": "kristineraymond@pyramax.com",
+        "phone": "+1 (952) 514-3112",
+        "address": "759 Monroe Street, Roulette, Oregon, 5493",
+        "about": "Dolor cupidatat sint officia officia duis cupidatat occaecat incididunt amet id voluptate excepteur. Qui sunt adipisicing eu dolore. Consectetur anim ut dolor ullamco irure.\r\n",
+        "registered": "2015-05-10T22:47:46 +04:00",
+        "latitude": -12.210565,
+        "longitude": -149.94047,
+        "tags": [
+            "irure",
+            "adipisicing",
+            "minim",
+            "deserunt",
+            "elit",
+            "do",
+            "occaecat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Janie Hamilton"
+            },
+            {
+                "id": 1,
+                "name": "Sykes Knight"
+            },
+            {
+                "id": 2,
+                "name": "Joy Mcgee"
+            }
+        ],
+        "greeting": "Hello, Kristine Raymond! You have 2 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "559444218bffa066076ced41",
+        "index": 1,
+        "guid": "1ea02971-06dc-4c23-b769-3e3e8e64db5a",
+        "isActive": true,
+        "balance": "$1,427.75",
+        "picture": "http://placehold.it/32x32",
+        "age": 39,
+        "eyeColor": "brown",
+        "name": "Doyle Horn",
+        "gender": "male",
+        "company": "MAGNINA",
+        "email": "doylehorn@magnina.com",
+        "phone": "+1 (978) 515-3231",
+        "address": "245 Newkirk Avenue, Clinton, Connecticut, 2057",
+        "about": "Sit ex ea occaecat consectetur occaecat est sit sunt ut dolor duis pariatur sit. Ipsum id Lorem minim sint quis esse enim non incididunt culpa culpa eiusmod do deserunt. Magna aliquip nisi eu ipsum laboris cillum occaecat ullamco et commodo dolore sunt ad cillum.\r\n",
+        "registered": "2014-08-08T03:06:33 +04:00",
+        "latitude": 0.993059,
+        "longitude": -26.23177,
+        "tags": [
+            "culpa",
+            "fugiat",
+            "elit",
+            "officia",
+            "ad",
+            "nisi",
+            "id"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Hartman Battle"
+            },
+            {
+                "id": 1,
+                "name": "Juarez Hoover"
+            },
+            {
+                "id": 2,
+                "name": "Mccarty Dalton"
+            }
+        ],
+        "greeting": "Hello, Doyle Horn! You have 9 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "559444213555a147a7e43695",
+        "index": 2,
+        "guid": "5f64f185-7214-43a0-8f9f-0c11ff668453",
+        "isActive": true,
+        "balance": "$1,149.32",
+        "picture": "http://placehold.it/32x32",
+        "age": 32,
+        "eyeColor": "blue",
+        "name": "Russell Mcfadden",
+        "gender": "male",
+        "company": "NEXGENE",
+        "email": "russellmcfadden@nexgene.com",
+        "phone": "+1 (883) 421-3811",
+        "address": "784 Bay Avenue, Zortman, Tennessee, 9031",
+        "about": "Id ex consequat est et sit Lorem cupidatat exercitation officia. Commodo minim voluptate incididunt labore quis cupidatat sunt. Irure ex ad fugiat non cupidatat aliqua ut aute deserunt non magna velit. Mollit et dolor laborum consequat cupidatat anim.\r\n",
+        "registered": "2015-02-05T04:38:49 +05:00",
+        "latitude": -74.052137,
+        "longitude": 8.904056,
+        "tags": [
+            "nostrud",
+            "nulla",
+            "aute",
+            "deserunt",
+            "sunt",
+            "in",
+            "occaecat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Sutton Beard"
+            },
+            {
+                "id": 1,
+                "name": "Mcintyre Walls"
+            },
+            {
+                "id": 2,
+                "name": "Davenport Mcmillan"
+            }
+        ],
+        "greeting": "Hello, Russell Mcfadden! You have 10 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "559444214bdfc8084e2dedf5",
+        "index": 3,
+        "guid": "162f69db-8740-4c5f-bc7f-6578a5bb5c65",
+        "isActive": true,
+        "balance": "$3,853.86",
+        "picture": "http://placehold.it/32x32",
+        "age": 36,
+        "eyeColor": "blue",
+        "name": "Witt Witt",
+        "gender": "male",
+        "company": "AEORA",
+        "email": "wittwitt@aeora.com",
+        "phone": "+1 (954) 437-3658",
+        "address": "639 Tompkins Avenue, Bentonville, Alabama, 3400",
+        "about": "Enim nostrud et voluptate officia minim voluptate. Nostrud veniam tempor non tempor amet irure. Amet culpa ut qui ipsum sunt enim commodo sit magna esse eiusmod officia dolor.\r\n",
+        "registered": "2014-10-28T13:56:20 +04:00",
+        "latitude": -81.393969,
+        "longitude": -147.746604,
+        "tags": [
+            "deserunt",
+            "id",
+            "non",
+            "incididunt",
+            "adipisicing",
+            "aliquip",
+            "nulla"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Clements Mcconnell"
+            },
+            {
+                "id": 1,
+                "name": "Vincent Briggs"
+            },
+            {
+                "id": 2,
+                "name": "Joann Wiggins"
+            }
+        ],
+        "greeting": "Hello, Witt Witt! You have 10 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "55944421a45178c2ac0e352e",
+        "index": 4,
+        "guid": "ae73edc8-720c-48e7-82f1-29edaf50f049",
+        "isActive": false,
+        "balance": "$2,776.19",
+        "picture": "http://placehold.it/32x32",
+        "age": 39,
+        "eyeColor": "green",
+        "name": "Valerie Gaines",
+        "gender": "female",
+        "company": "ENTOGROK",
+        "email": "valeriegaines@entogrok.com",
+        "phone": "+1 (899) 454-2609",
+        "address": "335 Losee Terrace, Galesville, Oklahoma, 1824",
+        "about": "Veniam in magna labore fugiat laborum do aliquip. Laborum officia magna ipsum fugiat aliqua id irure minim. Nostrud et id cupidatat reprehenderit. Dolor laboris do id voluptate incididunt eu minim ipsum voluptate culpa laboris minim. Occaecat Lorem ea in do occaecat tempor minim elit id esse commodo anim consequat.\r\n",
+        "registered": "2014-03-18T18:11:39 +04:00",
+        "latitude": -37.941964,
+        "longitude": -67.850815,
+        "tags": [
+            "reprehenderit",
+            "veniam",
+            "elit",
+            "sunt",
+            "id",
+            "magna",
+            "voluptate"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Paige Knowles"
+            },
+            {
+                "id": 1,
+                "name": "Ginger Chapman"
+            },
+            {
+                "id": 2,
+                "name": "Jerri Huffman"
+            }
+        ],
+        "greeting": "Hello, Valerie Gaines! You have 5 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "55944421ee3f7aa5bb3a3758",
+        "index": 5,
+        "guid": "3f3eed2b-35ee-4649-9214-baabb34e7138",
+        "isActive": false,
+        "balance": "$2,021.78",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "eyeColor": "blue",
+        "name": "Bradford Woodard",
+        "gender": "male",
+        "company": "HINWAY",
+        "email": "bradfordwoodard@hinway.com",
+        "phone": "+1 (811) 466-2745",
+        "address": "209 Polhemus Place, Homeworth, Missouri, 1822",
+        "about": "Qui deserunt esse dolore commodo sunt. Sunt officia amet commodo exercitation Lorem. Lorem irure laborum labore ut sint.\r\n",
+        "registered": "2014-07-16T08:52:00 +04:00",
+        "latitude": 74.839359,
+        "longitude": -14.879529,
+        "tags": [
+            "velit",
+            "cillum",
+            "pariatur",
+            "ea",
+            "officia",
+            "qui",
+            "esse"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Estelle Stuart"
+            },
+            {
+                "id": 1,
+                "name": "Mckay Burke"
+            },
+            {
+                "id": 2,
+                "name": "Watkins Higgins"
+            }
+        ],
+        "greeting": "Hello, Bradford Woodard! You have 4 unread messages.",
+        "favoriteFruit": "banana"
+    }
+]
---
+++ src/staticfile/integration/fake_supply_before_staticfile_test.go
@@ -0,0 +1,41 @@
+package integration_test
+
+import (
+	"path/filepath"
+
+	"github.com/cloudfoundry/libbuildpack/cutlass"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("running supply buildpacks before the staticfile buildpack", func() {
+	var app *cutlass.App
+	AfterEach(func() {
+		if app != nil {
+			app.Destroy()
+		}
+		app = nil
+	})
+
+	Context("the app is pushed once", func() {
+		BeforeEach(func() {
+			if ok, err := cutlass.ApiGreaterThan("2.65.1"); err != nil || !ok {
+				Skip("API version does not have multi-buildpack support")
+			}
+
+			app = cutlass.New(filepath.Join(bpDir, "fixtures", "fake_supply_staticfile_app"))
+			app.Buildpacks = []string{
+				"https://github.com/cloudfoundry/dotnet-core-buildpack#develop",
+				"staticfile_buildpack",
+			}
+			app.Disk = "1G"
+		})
+
+		It("finds the supplied dependency in the runtime container", func() {
+			PushAppAndConfirm(app)
+			Expect(app.Stdout.String()).To(ContainSubstring("Supplying Dotnet Core"))
+			Expect(app.GetBody("/")).To(ContainSubstring("This is an example app for Cloud Foundry that is only static HTML/JS/CSS assets."))
+		})
+	})
+})
