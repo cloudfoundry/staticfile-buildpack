46c21d8bc2a679a3d6b6e6cf818410918d5c694d
Tyler Phelan <tphelan@pivotal.io>
2018-12-03 15:12:14 -0500

    Allow integration test scripts to pass in buildpack zips
    - Pass in file paths as env vars
    - paths default to  for typical behavior
    
    Co-authored-by: Zander Mackie <amackie@pivotal.io>

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -9,10 +9,15 @@ GINKGO_NODES=${GINKGO_NODES:-3}
 GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-2}
 export CF_STACK=${CF_STACK:-cflinuxfs2}
 
+UNCACHED_BUILDPACK_FILE=${UNCACHED_BUILDPACK_FILE:-""}
+CACHED_BUILDPACK_FILE=${CACHED_BUILDPACK_FILE:-""}
+
 cd src/*/integration
 
 echo "Run Uncached Buildpack"
-ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached=false
+BUILDPACK_FILE="$UNCACHED_BUILDPACK_FILE" \
+  ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached=false
 
 echo "Run Cached Buildpack"
-ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached
+BUILDPACK_FILE="$CACHED_BUILDPACK_FILE" \
+  ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES --slowSpecThreshold=60 -- --cached
