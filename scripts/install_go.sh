3b5d0f786d0ca37b87dbe3f8689e58945ef1922c
Guillermo Kardolus <gkardolus@pivotal.io>
2019-01-22 16:31:49 -0500

    Use built version of go 1.11.4 in online unbuilt/URL buildpack [#163089977]
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
 set -euo pipefail
 
 GO_VERSION="1.11.4"
+
+if [ $CF_STACK == "cflinuxfs2" ]; then
+    GO_SHA256="8faf0b1823cf25416aa5ffdac6eef2f105114abe41b31b3d020102ae7661a5ae"
+elif [ $CF_STACK == "cflinuxfs3" ]; then
+    GO_SHA256="964b0b16d3a0b5ddf6705618537581e6ca7c101617a38ac0da4236685f0aa0ce"
+fi
 
 export GoInstallDir="/tmp/go$GO_VERSION"
 mkdir -p $GoInstallDir
 
 if [ ! -f $GoInstallDir/go/bin/go ]; then
-  GO_SHA256="fb26c30e6a04ad937bbc657a1b5bba92f80096af1e8ee6da6430c045a8db3a5b"
-  URL=https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+  URL=https://buildpacks.cloudfoundry.org/dependencies/go/go${GO_VERSION}.linux-amd64-${CF_STACK}-${GO_SHA256:0:8}.tar.gz
 
   echo "-----> Download go ${GO_VERSION}"
   curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/go.tar.gz
