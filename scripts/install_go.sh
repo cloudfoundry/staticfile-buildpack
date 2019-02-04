321d5c2467f6f8fd8d483ece06fa8012b250caf5
Zander Mackie <amackie@pivotal.io>
2019-02-04 17:24:07 -0500

    Update go to 1.11.5 for install_go
    [#163701450]

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-GO_VERSION="1.11.4"
+GO_VERSION="1.11.5"
 
 if [ $CF_STACK == "cflinuxfs2" ]; then
-    GO_SHA256="8faf0b1823cf25416aa5ffdac6eef2f105114abe41b31b3d020102ae7661a5ae"
+    GO_SHA256="51cab63f3de5e2f75a9036801712e4d7ae9bf226f0b61abce8d784e698148d3b"
 elif [ $CF_STACK == "cflinuxfs3" ]; then
-    GO_SHA256="964b0b16d3a0b5ddf6705618537581e6ca7c101617a38ac0da4236685f0aa0ce"
+    GO_SHA256="ee770df4e1863ee8e07574cb48e0245b61bec8f118faf6ec3742ea89eb20db28"
 fi
 
 export GoInstallDir="/tmp/go$GO_VERSION"
