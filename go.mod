50ddf9ef2f51cc372d7c84ee112e84245f0e597e
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2019-02-21 18:48:39 +0000

    Update libbuildpack

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/cloudfoundry/staticfile-buildpack
 
 require (
 	github.com/blang/semver v3.5.1+incompatible
-	github.com/cloudfoundry/libbuildpack v0.0.0-20190221181918-1aa00c1f7087
+	github.com/cloudfoundry/libbuildpack v0.0.0-20190221184352-b1cabf32f5c7
 	github.com/golang/mock v1.2.0
 	github.com/kardolus/httpmock v0.0.0-20181110092731-53def6cd0f87
 	github.com/onsi/ginkgo v1.7.0
--- vendor/github.com/cloudfoundry/libbuildpack/cutlass/cf.go
+++ vendor/github.com/cloudfoundry/libbuildpack/cutlass/cf.go
@@ -145,7 +145,13 @@ func DeleteBuildpack(language string) error {
 }
 
 func UpdateBuildpack(language, file, stack string) error {
-	command := exec.Command("cf", "update-buildpack", fmt.Sprintf("%s_buildpack", language), "-p", file, "--enable", "-s", stack)
+	updateBuildpackArgs := []string{"update-buildpack", fmt.Sprintf("%s_buildpack", language), "-p", file, "--enable"}
+
+	if stack != "" {
+		updateBuildpackArgs = append(updateBuildpackArgs, "-s", stack)
+	}
+
+	command := exec.Command("cf", updateBuildpackArgs...)
 	if data, err := command.CombinedOutput(); err != nil {
 		return fmt.Errorf("Failed to update buildpack by running '%s':\n%s\n%v", strings.Join(command.Args, " "), string(data), err)
 	}
