eeeeb6a81a36ba9b5f2048770dcc85db02b88152
Zander Mackie <amackie@pivotal.io>
2019-10-24 08:59:44 -0400

    Correct example.com text to be less change sensitive

--- src/staticfile/integration/deploy_pushstate_and_proxy_pass_test.go
+++ src/staticfile/integration/deploy_pushstate_and_proxy_pass_test.go
@@ -29,7 +29,7 @@ var _ = Describe("deploy a pushstate and reverse proxy app", func() {
 		})
 
 		By("proxies", func() {
-			Expect(app.GetBody("/api")).To(ContainSubstring("This domain is established to be used for illustrative examples in documents"))
+			Expect(app.GetBody("/api")).To(ContainSubstring("illustrative examples in documents"))
 		})
 	})
 })
