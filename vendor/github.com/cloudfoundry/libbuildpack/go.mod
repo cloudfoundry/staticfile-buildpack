dcfa9b6e37cf883bd7d14256302af600f0641d16
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2020-03-27 21:11:42 +0000

    Update libbuildpack

--- vendor/github.com/cloudfoundry/libbuildpack/go.mod
+++ vendor/github.com/cloudfoundry/libbuildpack/go.mod
@@ -4,7 +4,7 @@ require (
 	code.cloudfoundry.org/lager v2.0.0+incompatible
 	github.com/Masterminds/semver v1.5.0
 	github.com/blang/semver v3.5.1+incompatible
-	github.com/cloudfoundry/packit v0.0.0-20200213185904-9d0fc689e7af
+	github.com/cloudfoundry/packit v0.0.2
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/elazarl/goproxy v0.0.0-20190911111923-ecfe977594f1
 	github.com/elazarl/goproxy/ext v0.0.0-20190911111923-ecfe977594f1 // indirect
@@ -13,7 +13,7 @@ require (
 	github.com/jarcoal/httpmock v1.0.5
 	github.com/onsi/ginkgo v1.12.0
 	github.com/onsi/gomega v1.9.0
-	github.com/pkg/errors v0.8.1
+	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/tidwall/gjson v1.6.0
 	gopkg.in/yaml.v2 v2.2.8
--- vendor/github.com/cloudfoundry/packit/pexec/executable.go
+++ vendor/github.com/cloudfoundry/packit/pexec/executable.go
@@ -5,24 +5,22 @@ import (
 	"os/exec"
 )
 
+// Executable represents an executable on the $PATH.
 type Executable struct {
 	name string
 }
 
-type Execution struct {
-	Args   []string
-	Dir    string
-	Env    []string
-	Stdout io.Writer
-	Stderr io.Writer
-}
-
+// NewExecutable returns an instance of an Executable given the name of that
+// executable. When given simply a name, the execuable will be looked up on the
+// $PATH before execution. Alternatively, when given a path, the executable
+// will use that path to invoke the executable file directly.
 func NewExecutable(name string) Executable {
 	return Executable{
 		name: name,
 	}
 }
 
+// Execute invokes the executable with a set of Execution arguments.
 func (e Executable) Execute(execution Execution) error {
 	path, err := exec.LookPath(e.name)
 	if err != nil {
@@ -43,4 +41,26 @@ func (e Executable) Execute(execution Execution) error {
 	cmd.Stderr = execution.Stderr
 
 	return cmd.Run()
+}
+
+// Execution is the set of configurable options for a given execution of the
+// executable.
+type Execution struct {
+	// Args is a list of the arguments to be passed to the executable.
+	Args []string
+
+	// Dir is the path to a directory from with the executable should be invoked.
+	// If Dir is not set, the current working directory will be used.
+	Dir string
+
+	// Env is the set of environment variables that make up the environment for
+	// the execution. If Env is not set, the existing os.Environ value will be
+	// used.
+	Env []string
+
+	// Stdout is where the output of stdout will be written during the execution.
+	Stdout io.Writer
+
+	// Stderr is where the output of stderr will be written during the execution.
+	Stderr io.Writer
 }
