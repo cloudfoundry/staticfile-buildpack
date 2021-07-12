4115270a8c2c6eeb96ecf70b1cd3df37b9d81378
Florian Arthofer <florian.arthofer@dynatrace.com>
2021-07-12 12:43:39 +0200

    bumped libbuildpack-dynatrace to v1.4.0 and update integration tests with networkzones

--- src/staticfile/integration/deploy_app_with_dynatrace_test.go
+++ src/staticfile/integration/deploy_app_with_dynatrace_test.go
@@ -79,6 +79,31 @@ var _ = Describe("CF Staticfile Buildpack", func() {
 		})
 	})
 
+	Context("deploying a staticfile app with Dynatrace agent with configured network zone", func() {
+		It("checks if Dynatrace injection was successful", func() {
+			serviceName := "dynatrace-" + cutlass.RandStringRunes(20) + "-service"
+			command := exec.Command("cf", "cups", serviceName, "-p", fmt.Sprintf("'{\"apitoken\":\"secretpaastoken\",\"apiurl\":\"%s\",\"environmentid\":\"envid\", \"networkzone\":\"testzone\"}'", dynatraceAPIURI))
+			_, err := command.CombinedOutput()
+			Expect(err).To(BeNil())
+			createdServices = append(createdServices, serviceName)
+
+			command = exec.Command("cf", "bind-service", app.Name, serviceName)
+			_, err = command.CombinedOutput()
+			Expect(err).To(BeNil())
+			command = exec.Command("cf", "restage", app.Name)
+			_, err = command.Output()
+			Expect(err).To(BeNil())
+
+			Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
+			Expect(app.Stdout.String()).To(ContainSubstring("Dynatrace service credentials found. Setting up Dynatrace OneAgent."))
+			Expect(app.Stdout.String()).To(ContainSubstring("Starting Dynatrace OneAgent installer"))
+			Expect(app.Stdout.String()).To(ContainSubstring("Copy dynatrace-env.sh"))
+			Expect(app.Stdout.String()).To(ContainSubstring("Setting DT_NETWORK_ZONE..."))
+			Expect(app.Stdout.String()).To(ContainSubstring("Dynatrace OneAgent installed."))
+			Expect(app.Stdout.String()).To(ContainSubstring("Dynatrace OneAgent injection is set up."))
+		})
+	})
+
 	Context("deploying a staticfile app with Dynatrace agent with two credentials services", func() {
 		It("checks if detection of second service with credentials works", func() {
 			credentialsServiceName := "dynatrace-" + cutlass.RandStringRunes(20) + "-service"
